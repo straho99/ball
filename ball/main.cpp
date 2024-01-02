a9750609a79a4065d69aac896913f35e9868bf3d
StrahilRuychev <strahil.ruychev@gmail.com>
2024-01-02 10:27:28 +0200

    Initial Commit

---
+++ ball/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ball
+//
+//  Created by Strahil Ruychev on 2.01.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
