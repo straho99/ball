402d5f4f957253589ecad43d2f0ff4b36f268659
StrahilRuychev <strahil.ruychev@gmail.com>
2024-01-02 10:56:05 +0200

    Ball

--- ball/main.cpp
+++ ball/main.cpp
@@ -1,14 +1,37 @@
-//
-//  main.cpp
-//  ball
-//
-//  Created by Strahil Ruychev on 2.01.24.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    long long n;
+    int position;
+    
+    cin >> n;
+    cin >> position;
+    
+    n = n % 6;
+    
+    for (long long i = n; i >= 1; i--) {
+        if (i % 2 != 0) {
+            if (position != 2) {
+                if (position == 0) {
+                    position = 1;
+                } else {
+                    position = 0;
+                }
+            }
+        } else {
+            if (position != 0) {
+                if (position == 1) {
+                    position = 2;
+                } else {
+                    position = 1;
+                }
+            }
+        }
+    }
+    
+    cout << position;
+    
     return 0;
 }
